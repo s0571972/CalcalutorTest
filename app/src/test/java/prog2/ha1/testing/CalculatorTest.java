09cbaebf4177234b14571b39b9547000babb8cd5
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2021-04-12 23:07:34 +0200

    a calculator, buggy and poorly tested

---
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -0,0 +1,20 @@
+package prog2.ha1.testing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Retro calculator")
+class CalculatorTest {
+
+    @Test
+    @DisplayName("should display result after adding two positive numbers")
+    void calculatorCanDoTwoPlusTwo() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(2);
+        calc.pressEquals();
+        assertEquals("4", calc.readScreen());
+    }
+}

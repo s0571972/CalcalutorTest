547f264e45cc665e2b830ada53025125af76cc3f
gladys <s0571972@htw-berlin.de>
2021-04-25 19:23:27 +0200

    2 new Test Example was added but had 2 error

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -52,5 +52,37 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
+	
+	@Test
+    @DisplayName("should display result after Multiplication two positive numbers")
+    void testPositiveMultiplication() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(4);
+        calc.pressBinaryOperationKey("*");
+        calc.pressDigitKey(2);
+
+        calc.pressEqualsKey();
+        String expected = "8";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("should display result after Division two positive numbers")
+    void testPositiveDivision() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(4);
+        calc.pressBinaryOperationKey("?");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "invalid";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
